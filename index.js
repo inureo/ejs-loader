d32745f2207c37d074be53d791734fb34348cbc7
inureo <ryo_tezuka@r.recruit.co.jp>
2016-02-14 01:09:29 +0900

    use htmlclean for minify

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var _ = require('lodash');
-var loaderUtils = require("loader-utils");
+var loaderUtils = require('loader-utils');
+var htmlClean = require('htmlclean');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var options = loaderUtils.parseQuery(this.query);
-  var template = _.template(source, options);
+  var template = _.template(htmlClean(source), options);
   return 'module.exports = ' + template;
 };
